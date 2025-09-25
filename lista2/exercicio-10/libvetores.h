f41b3322970aa597ebc4e5f9aaac5238779cb8e2
Marcelo Lopes Cairuga <marcelocairuga@ifsul.edu.br>
2025-09-25 11:18:57 -0300

    Add header file libvetores.h with function declarations

---
+++ lista2/exercicio-10/libvetores.h
@@ -0,0 +1,31 @@
+#ifndef LIBVETORES_H
+#define LIBVETORES_H
+
+// Exercício 01
+void imprimir(float vetor[], int n);
+
+// Exercício 02
+void imprimirInvertido(int vetor[], int n);
+
+// Exercício 03
+void real(float valor);
+
+// Exercício 04
+void imprimirFormatado(float vetor[], int n);
+
+// Exercício 05
+int pares(int vetor[], int n);
+
+// Exercício 06
+int impares(int vetor[], int n);
+
+// Exercício 07
+float mediaPonderada(float notas[], int pesos[], int n);
+
+// Exercício 08
+float central(float vetor[], int n);
+
+// Exercício 09
+float produtoEscalar(float v1[], float v2[], int n);
+
+#endif
